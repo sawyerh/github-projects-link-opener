c572eaa05a270313d52884a3dcfaa3706a71713f
Sawyer <git@sawyerh.com>
2023-10-03 14:11:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GitHub Projects link opener
+
+Stops GitHub Projects default behavior of opening links in an in-page panel, and instead opens the Issue permalink page.
---
+++ scripts.js
@@ -0,0 +1,14 @@
+function openIssue(url) {
+  history.replaceState(null, "", url);
+  window.location.href = url;
+}
+
+document.addEventListener("click", (event) => {
+  const link = event.target.closest('[data-testid="card-side-panel-trigger"]');
+  if (!link) return;
+
+  const href = link.href;
+  if (href.includes("/issues/")) {
+    openIssue(href);
+  }
+});
